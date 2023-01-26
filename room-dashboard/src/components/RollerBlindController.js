0070fcaabcdcbf50c04fdbfdf4fef9368eaf86e5
Filippo Sanzani <filippo.sanzani2001@gmail.com>
2023-01-26 17:37:36 +0100

    Add update value to roller blinds controller.

--- room-dashboard/src/components/RollerBlindController.js
+++ room-dashboard/src/components/RollerBlindController.js
@@ -3,10 +3,16 @@ import axios from 'axios';
 
 async function RollerBlindController() {
   const [value, setValue] = useState(-1);
-  const value_endpoint = 'http://localhost:1234/rollerblind/state' 
+  const value_endpoint = 'http://localhost:1234/rollerblind/state';
+
+  const update_value = async function(evt) {
+    const new_value = evt.target.value;
+    await axios.post(value_endpoint, { value: new_value });
+    setValue(new_value);
+  }
 
   useEffect(() => {
-    async function getRollerBlindState() {
+    const getRollerBlindState = async function () {
       const response = await axios.get(value_endpoint);
       const data = await response.json();
       setValue(data.value);
@@ -16,12 +22,12 @@ async function RollerBlindController() {
       getRollerBlindState();
     }
   }, []);
-
+  
   return (
     <div>
       <form>
         <label for="roller-blind-controller"> Roller Blinds: </label>
-        <input id="roller-blind-controller" type="range" min="0" max="100" name="roller-blind-controller" value={{ value }} />
+        <input id="roller-blind-controller" type="range" min="0" max="100" name="roller-blind-controller" value={ value } onChange={ update_value } />
       </form>
     </div>
   )
