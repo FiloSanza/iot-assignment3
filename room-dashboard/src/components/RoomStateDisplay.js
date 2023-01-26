88ef1f4ab5131eedba4e8c0ca20aeae28a3018d5
Filippo Sanzani <filippo.sanzani2001@gmail.com>
2023-01-26 17:55:50 +0100

    Add room state component.

---
+++ room-dashboard/src/components/RoomStateDisplay.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from 'react';
+import LightState from './LightState';
+import axios from 'axios';
+
+function RoomStateDisplay() {
+  const [room_state, setRoomState] = useState(null);
+  const room_state_endpoint = 'http://localhost:1234/room/state';
+
+  useEffect(() => {
+    const getRoomState = async function () {
+      const response = await axios.get(room_state_endpoint);
+      const data = await response.json();
+      setRoomState(data);
+    }
+
+    if (value === null) {
+      getRoomState();
+    }
+  }, []);
+
+  return (
+    <div>
+      <p> The light is: <LightState light_on={room_state.light_on} /> </p> 
+      <p> The room is currently: <span> { room_state.empty ? 'empty' : 'occupied' } </span> </p>
+    </div>
+  )
+}
+
+export default RoomStateDisplay;
