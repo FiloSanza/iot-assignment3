ae51e61d765ec4fe0ea6c5263c3cd5c2725e6772
Filippo Sanzani <filippo.sanzani2001@gmail.com>
2023-01-26 16:48:48 +0100

    Add main implementation.

--- room-sensor-board/lib/utils/consts.h
+++ room-sensor-board/lib/utils/consts.h
@@ -3,6 +3,10 @@
 
 #include "types.h"
 
+// Wifi
+constexpr char* WIFI_SSID = "...";
+constexpr char* WIFI_PASSWORD = "...";
+
 // Light Sensor
 constexpr int VIN = 5;
 constexpr int LIGHT_SENSOR_RESITOR = 1000; // ms
@@ -12,8 +16,15 @@ constexpr int MAX_NUMBER_OF_TASKS = 4;
 constexpr int UPDATE_VALUES_PERIOD = 1000; // ms
 
 // Pins
+constexpr pin_t LED_PIN = 3; 
+constexpr pin_t PIR_PIN = 4; 
+constexpr pin_t LIGHT_SENSOR_PIN = 5; 
 
 // Comm pool
 constexpr int MAX_NUMBER_OF_COMMS = 4;
+
+// MQTT
+constexpr char* MQTT_SERVER = "...";
+constexpr char* MQTT_TOPIC = "...";
 
 #endif
--- room-sensor-board/src/main.cpp
+++ room-sensor-board/src/main.cpp
@@ -1,9 +1,34 @@
 #include <Arduino.h>
 
+#include "components.h"
+#include "scheduler.h"
+#include "tasks/update_values.h"
+#include "comm_pool.h"
+#include "mqtt.h"
+#include "wifi.h"
+
+Comms::CommPool* comm_pool;
+Components::Led* led;
+Components::Pir* pir;
+Components::LightSensor* light_sensor;
+Scheduling::Scheduler* scheduler;
+
 void setup() {
-  // put your setup code here, to run once:
+    Serial.begin(9600);
+
+    Comms::WiFiClient::connect(WIFI_SSID, WIFI_PASSWORD);
+
+    comm_pool = new Comms::CommPool();
+    comm_pool->addRecv(new Comms::MqttRecv(MQTT_SERVER, MQTT_TOPIC));
+
+    led = new Components::Led(LED_PIN);
+    pir = new Components::Pir(PIR_PIN);
+    light_sensor = new Components::LightSensor(LIGHT_SENSOR_PIN);
+
+    scheduler = new Scheduling::Scheduler();
+    scheduler->schedule(new Tasks::UpdateValues(comm_pool, led, pir, light_sensor));
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    scheduler->tick();
 }
