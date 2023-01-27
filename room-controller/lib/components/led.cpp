305ea8abf7502cf4749574f754f2a10b15ebea89
Lorenzo Drudi <lorenzodrudi11@gmail.com>
2023-01-27 15:32:30 +0100

    Room-Controller: add led.

---
+++ room-controller/lib/components/led.cpp
@@ -0,0 +1,26 @@
+#include "led.h"
+
+namespace Components {
+    Led::Led() {}
+    Led::Led(pin_t pin) : pin(pin) {
+        pinMode(pin, OUTPUT);
+    }
+
+    void Led::turnOn() {
+        if (!is_on) {
+            is_on = true;
+            digitalWrite(pin, HIGH);
+        }
+    }
+
+    void Led::turnOff() {
+         if (is_on) {
+            is_on = false;
+            digitalWrite(pin, LOW);
+        }
+    }
+
+    bool Led::isOn() const {
+        return is_on;
+    }
+}
