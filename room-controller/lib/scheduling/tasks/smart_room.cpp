140c57637b66f3a57c175edf55052aebc2c43f47
Lorenzo Drudi <lorenzodrudi11@gmail.com>
2023-01-31 19:06:40 +0100

    Room-Controller: add smart_room task impl.

--- room-controller/lib/scheduling/tasks/smart_room.cpp
+++ room-controller/lib/scheduling/tasks/smart_room.cpp
@@ -24,15 +24,14 @@ namespace Tasks {
 
         markExecutedNow();
 
-        // if (MsgServiceBT.isMsgAvailable()) {
-        //     Msg* msg = MsgServiceBT.receiveMsg();    
-        //     updateRoom(msg);
-        // }
+        if (MsgServiceBT.isMsgAvailable()) {
+            Msg* msg = MsgServiceBT.receiveMsg();    
+            updateRoom(msg);
+        }
 
         if (MsgService.isMsgAvailable()) {
-            lighting_subsystem->turnOn();
-            // Msg* msg = MsgService.receiveMsg();  
-            // updateRoom(msg);
+            Msg* msg = MsgService.receiveMsg();  
+            updateRoom(msg);
         }
     }
 
@@ -40,22 +39,27 @@ namespace Tasks {
         Logger::Message log_msg;
 
         String json = msg->getContent();
-        DynamicJsonDocument doc(1024);
-        deserializeJson(doc, json);
-
+        DynamicJsonDocument doc(512);
+        DeserializationError error = deserializeJson(doc, json);
+        if (error) {
+            Serial.print(F("deserializeJson() failed: "));
+            Serial.println(error.f_str());
+        }
         if (doc.containsKey("light")) {
-            if (doc["light"] == "1") {
+            if (doc["light"] == 1) {
                 lighting_subsystem->turnOn();
-                log_msg.setTag(SmartRoomMessageTag::LightOn);
             } else {
                 lighting_subsystem->turnOff();
-                log_msg.setTag(SmartRoomMessageTag::LightOff);
             }
-            log_msg.setDescription("Lightning subsystem update").log();
-        } else if (doc.containsKey("angle")) {
-            lighting_subsystem->turnOff();
-            log_msg.setTag(SmartRoomMessageTag::LightOff)
+            log_msg.setTag(SmartRoomMessageTag::Light)
+                .setData(doc["light"])
                 .setDescription("Lightning subsystem update")
+                .log();
+        } else if (doc.containsKey("angle")) {
+            roller_blinds->moveTo(doc["angle"]);
+            log_msg.setTag(SmartRoomMessageTag::Valve)
+                .setData(doc["angle"])
+                .setDescription("Roller blinds update")
                 .log();
         }
     }
--- room-controller/lib/utils/types.h
+++ room-controller/lib/utils/types.h
@@ -8,8 +8,8 @@ typedef uint8_t pin_t;
 typedef unsigned long timestamp_t;
 
 enum SmartRoomMessageTag : uint32_t {
-    LightOff = 0,
-    LightOn = 1
+    Light = 0,
+    Valve = 1
 };
 
 #endif
