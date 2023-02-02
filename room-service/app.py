8a0425a7cdfd09b1451ebb8ad81e8acff7a197c8
Lorenzo Drudi <lorenzodrudi11@gmail.com>
2023-02-02 17:29:45 +0100

    Room-Service: fix.

--- room-service/app.py
+++ room-service/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from flask_cors import CORS
+from flask_mqtt import Mqtt
 import arduino_comm
 from datetime import datetime
 from consts import *
@@ -9,13 +10,40 @@ import json
 
 app = Flask(__name__)
 CORS(app)
+
+# Mqtt configuration
+app.config['MQTT_BROKER_URL'] = 'localhost' # See https://mosquitto.org/
+app.config['MQTT_BROKER_PORT'] = 1883
+app.config['MQTT_KEEPALIVE'] = 5  # Set KeepAlive time in seconds
+app.config['MQTT_TLS_ENABLED'] = False  # If your server supports TLS, set it True
+topic = 'smart-room'
+mqtt_client = Mqtt(app)
+
+# Connect Callback function
+@mqtt_client.on_connect()
+def handle_connect(client, userdata, flags, rc):
+   if rc == 0:
+       print('Connected successfully')
+       mqtt_client.subscribe(topic) # subscribe topic
+   else:
+       print('Bad connection. Code:', rc)
+       
+# Message callback function 
+@mqtt_client.on_message()
+def handle_mqtt_message(client, userdata, message):
+   data = dict(
+       topic=message.topic,
+       payload=message.payload.decode()
+  )
+   print('Received message on topic: {topic} with payload: {payload}'.format(**data))
+   
 arduino = arduino_comm.SerialLine(port="/dev/ttyACM0", baudrate=9600, timeout=0.1)
 
 lock = threading.Lock()
 data = {
     LIGHT_LOGS: [{
         'state': 0,
-        'ts': datetime.now()
+        'ts': datetime.now().strftime("%d/%m/%Y %H:%M:%S")
     }],
     PIR_STATE: 0,
     LIGHT_STATE: 0,
@@ -32,7 +60,7 @@ def update_data():
             if LIGHT_STATE in msg and msg[LIGHT_STATE] != data[LIGHT_STATE]:
                 data[LIGHT_LOGS].append({
                     'state': msg[LIGHT_STATE],
-                    'ts': datetime.now()
+                    'ts': datetime.now().strftime("%d/%m/%Y %H:%M:%S")
                 })
                 data[LIGHT_STATE] = msg[LIGHT_STATE]
             # The rollerblind state changed
@@ -61,7 +89,7 @@ def update_light():
     if state != data[LIGHT_STATE]:
         data[LIGHT_LOGS].append({
             'state': state,
-            'ts': datetime.now()
+            'ts': datetime.now().strftime("%d/%m/%Y %H:%M:%S")
         })
         data[LIGHT_STATE] = state
 
